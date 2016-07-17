1198aa1e9cad9f333734c738cb7b452be430ab1b
Giovanni Grieco <giovanni.grc96@gmail.com>
2016-07-17 14:54:35 +0200

    First public commit

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # DiteCross
 Discord-Telegram Cross messaging system
+
+![Discord Example](/readme_assets/discord.PNG)
+![Telegram Example](/readme_assets/telegram.PNG)
+
+This program is just a proof of concept to demonstrate the possibility to relay Discord and Telegram messages from one group to another.
+
+## Installation
+DiteCross is based on NodeJS (so make sure to have it) and relies upon some dependencies. Follow these commands to install them with `npm`:
+
+```
+npm install --save --no-optional discord.js
+npm install --save node-telegram-bot-api
+npm install --save readline
+```
+
+## Getting started
+Make sure to have both **token**s to login to Telegram and Discord.
+
+### How to register a Telegram token
+For Telegram you have to talk with @BotFather and follow the procedure to make a new bot. **DiteCross_bot** name is already taken by me.
+The token should be placed in a `telegram.ini` file like below:
+
+```
+[token]
+PLACE_TOKEN_HERE
+```
+
+### How to register a Discord token
+For Discord you have to create a new account and place the token in a `discord.ini` file, like you did for Telegram.
+
+In case you don't know how to get a Discord token, just place in `discord.ini` file DiteCross' email and password. They will be replaced at first login with the correct token. Follow this model:
+
+```
+[email]
+PLACE_EMAIL_HERE
+PLACE_PASSWORD_HERE
+```
+
+## First time run
+To run DiteCross just type `node ditecross.js`
+
+If everything works correctly, you should have successful messages at startup as described below:
+
+```
+DiteCross - Made by corsaro
+[Telegram] Login successful
+[Discord] Login successful
+```
+
+Make sure that DiteCross bots are both in **ONE** discord channel and **ONE** Telegram group or private channel. Make a "test message" on both ends so that DiteCross can understand where to relay the messages.
+
+**Remember that this is a proof of concept, not a end-user and secure final program.**
+
+That's it, enjoy! If you have questions / problems / whatsoever, feel free to open an issue here on GitHub or contact me via email (see my profile).
---
+++ ditecross.js
@@ -0,0 +1,89 @@
+console.log("DiteCross - Made by corsaro");
+
+const fs = require('fs');
+
+const TelegramBot = require('node-telegram-bot-api');
+const Discord = require("discord.js");
+const discord_ditecross = new Discord.Client();
+var telegram_ditecross = undefined;
+
+var discord_ditecross_serverid = '';
+var telegram_ditecross_serverid = '';
+
+discord_ditecross.on("message", function(message)
+{
+	//console.log('[' + message.author.username + '@' + message.server.name + '|Discord]> ' + message.content);
+	discord_ditecross_serverid = message.channel.id;
+	if (message.author.username !== "DiteCross")
+		discord_relay_to_telegram(message.author.username, message.server.name, message.content);
+});
+
+discord_login_via_file();
+telegram_login_via_file();
+
+function discord_login_via_file() {
+	var fields = fs.readFileSync('discord.ini').toString().split('\n');
+	if (fields[0] == "[token]") {
+		discord_ditecross.loginWithToken(fields[1]).then(discord_loginSuccess).catch(loginError);
+	} else if (fields[0] == "[email]") {
+		discord_ditecross.login(fields[1], fields[2]).then(discord_loginSuccess).catch(loginError);
+	} else {
+		loginError('Discord login data not found in file');
+	}
+}
+
+function discord_loginSuccess(token)
+{
+	console.log('[Discord] Login successful');
+	fs.writeFile('discord.ini', '[token]\n' + token, (error) => {
+		if (error)
+			console.log('[Discord] Error saving token to file.');
+	});
+}
+
+function discord_relay_to_telegram(username, serverName, content) {
+	if (telegram_ditecross_serverid) {
+		message = '[' + username + '@' + serverName + ']> ' + content
+		telegram_ditecross.sendMessage(telegram_ditecross_serverid, message);
+		console.log('[Discord>>Telegram] Message sent');
+	}
+}
+
+function loginError(error)
+{
+	console.log('Error: ' + error);
+	console.log('Abort');
+	process.exit(1);
+}
+
+function telegram_login_via_file() {
+	var fields = fs.readFileSync('telegram.ini').toString().split('\n');
+	if (fields[0] == "[token]") {
+		telegram_initialise(fields[1]);
+	} else {
+		loginError('Telegram token not found in file');
+	}
+}
+
+function telegram_initialise(token) {
+	telegram_ditecross = new TelegramBot(token, {polling: true});
+
+	console.log('[Telegram] Login successful');
+
+	telegram_ditecross.on('message', function(message) {
+		//console.log('[' + message.from.username + "@" + message.chat.title + '|Telegram]> ' + message.text);
+		telegram_ditecross_serverid = message.chat.id;
+		telegram_relay_to_discord(message.from.username, message.chat.title, message.text);
+	});
+}
+
+function telegram_relay_to_discord(username, serverName, content) {
+	if (discord_ditecross_serverid) {
+		message = '[' + username + '@' + serverName + ']> ' + content;
+		discord_ditecross.sendMessage(discord_ditecross_serverid, message).then(function (message) {
+			console.log('[Telegram>>Discord] Message sent');
+		}).catch(function (error) {
+			console.log('[Telegram>>Discord] Message error');
+		});
+	}
+}
