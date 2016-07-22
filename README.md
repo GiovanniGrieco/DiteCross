1259d591db316420b2e84233145a3073496572b8
Giovanni Grieco <giovanni.grc96@gmail.com>
2016-07-22 22:29:07 +0200

    Not a Proof of Concept Anymore!
    - Rewritten from the ground up
    - Changed API from discord.js to discord.io
    - Changed License from MIT to GPLv3
    - Updated README
    - Updated .gitignore

--- README.md
+++ README.md
@@ -3,20 +3,17 @@ Discord-Telegram Cross messaging system
 
 ![DiteCross Example](/readme_assets/example.png)
 
-This program demonstrates the possibility to relay Discord and Telegram messages from one group to another.
+A bot that automatically relays Discord and Telegram messages from one group to another.
 
 ## Installation
-DiteCross is based on NodeJS with `npm`, so make sure to have both of them installed. To install DiteCross just type
-
-```
-npm install
-```
+DiteCross is based on NodeJS with `npm`, so make sure to have both of them installed.
+To install DiteCross just type `npm install`
 
 ## Getting started
 Make sure to have both **token**s to login to Telegram and Discord.
 
 ### How to register a Telegram token
-For Telegram you have to talk with @BotFather and follow the procedure to make a new bot. **DiteCross_bot** name is already taken by me.
+For Telegram you have to talk with @BotFather and follow the procedure to make a new bot. [@DiteCross_bot](https://telegram.me/DiteCross_bot) is the official one (not 24/7 yet).
 The token should be placed in a `telegram.ini` file like below:
 
 ```
@@ -27,19 +24,17 @@ PLACE_TOKEN_HERE
 ### How to register a Discord token
 For Discord you have to create a new User Bot and place the token in a `discord.ini` file, like you did for Telegram.
 
-## First time run
+## First time run and handshake
 To run DiteCross just type `node ditecross.js`
 
-If everything works correctly, you should have successful messages at startup as described below:
+The handshake process is described below:
+1. Type `/handshake` on Telegram
+2. DiteCross asks you to insert Discord server and channel names, like this: `@ServerName#ChannelName`
+3. If DiteCross is present on that server and channel, it will ask you to type `/handshake` there; do it
+4. Enjoy!
 
-```
-DiteCross - Made by corsaro
-[Telegram] Login successful
-[Discord] Login successful
-```
+NOTE: I hope to have built a robust and simple handshaking system. If you have one better in mind, please let me know!
 
-Make sure that DiteCross bots are both in **ONE** discord channel and **ONE** Telegram group or private channel. Make a "test message" on both ends so that DiteCross can understand where to relay the messages.
-
-**Be aware that DiteCross is not stable and secure, yet.**
+**Be aware that DiteCross is not stable, yet.**
 
 That's it, enjoy! If you have questions / problems / whatsoever, feel free to open an issue here on GitHub or contact me via email (see my profile).
--- ditecross.js
+++ ditecross.js
@@ -1,87 +1,270 @@
-console.log("DiteCross - Made by corsaro");
+/*
+DiteCross - Discord-Telegram Cross messaging system
+Copyright (C) 2015 Giovanni Grieco
 
-const fs = require('fs');
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-const TelegramBot = require('node-telegram-bot-api');
-const Discord = require("discord.js");
-const discord_ditecross = new Discord.Client();
-var telegram_ditecross = undefined;
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-var discord_ditecross_serverid = '';
-var telegram_ditecross_serverid = '';
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+console.log("DiteCross\tCopyright (C) 2015  Giovanni Grieco")
+/*
+ * GLOBAL DEPENDENCIES
+ */
+const fs = require('fs')
 
-discord_ditecross.on("message", function(message)
-{
-	//console.log('[' + message.author.username + '@' + message.server.name + '|Discord]> ' + message.content);
-	discord_ditecross_serverid = message.channel.id;
-	if (message.author.username !== "DiteCross")
-		discord_relay_to_telegram(message.author.username, message.server.name, message.content);
-});
+/*
+ * BASIC FUNCTIONS AND UTILITIES
+ */
+function read_token(fileName) {
+	var lines = fs.readFileSync(fileName).toString().split('\n')
+	if (lines[0] === "[token]" && lines[1] != undefined)
+		return lines[1]
+	else
+		throwError("ERROR: Token not found in file " + fileName)
+}
 
-discord_login_via_file();
-telegram_login_via_file();
+function throwError(error) {
+	console.error(error)
+	process.exit(1)
+}
 
-function discord_login_via_file() {
-	var fields = fs.readFileSync('discord.ini').toString().split('\n');
-	if (fields[0] == "[token]") {
-		discord_ditecross.loginWithToken(fields[1]).then(discord_loginSuccess).catch(loginError);
-	} else {
-		loginError('Discord token not found in file');
+/*
+ * INIT
+ */
+init();
+function init() {
+	const DISCORD_TOKEN  = read_token('discord.ini')
+	const TELEGRAM_TOKEN = read_token('telegram.ini')
+
+	var Bot = new DiteCross(DISCORD_TOKEN, TELEGRAM_TOKEN)
+
+	process.on('SIGINT', function () {
+		Bot.saveCrossTable()
+		process.exit(0)
+	})
+}
+
+/*
+ * DITECROSS
+ */
+function DiteCross(Discord_Token, Telegram_Token) {
+	/*
+	 * DISCORD
+	 */
+	this.Discord_initialise = function (token) {
+		this.Discord_Bot = new this.Discord_API.Client({token: token, autorun: true})
+		this.Discord_Bot.on('message', (user, userID, channelID, message, event) => this.Discord_messagePoll(user, userID, channelID, message, event))
 	}
-}
 
-function discord_loginSuccess(token)
-{
-	console.log('[Discord] Login successful');
-	fs.writeFile('discord.ini', '[token]\n' + token, (error) => {
-		if (error)
-			console.log('[Discord] Error saving token to file.');
-	});
-}
-
-function discord_relay_to_telegram(username, serverName, content) {
-	if (telegram_ditecross_serverid) {
-		message = '[' + username + '@' + serverName + ']> ' + content
-		telegram_ditecross.sendMessage(telegram_ditecross_serverid, message);
-		console.log('[Discord>>Telegram] Message sent');
+	this.Discord_messagePoll = function (user, userID, channelID, message, event) {
+		if (message.charAt(0) === '/')
+			this.Discord_manageCommand(userID, channelID, message)
+		else
+			this.Discord_manageMessage(userID, channelID, message)
 	}
-}
 
-function loginError(error)
-{
-	console.log('Error: ' + error);
-	console.log('Abort');
-	process.exit(1);
-}
-
-function telegram_login_via_file() {
-	var fields = fs.readFileSync('telegram.ini').toString().split('\n');
-	if (fields[0] == "[token]") {
-		telegram_initialise(fields[1]);
-	} else {
-		loginError('Telegram token not found in file');
+	this.Discord_manageCommand = function (userID, channelID, message) {
+		if (message.startsWith('handshake', 1)) {
+			this.Discord_processHandshake(userID, channelID)
+		} else if (message.startsWith('help', 1)) {
+			var ret = 'I relay your message for you on Telegram or help you know what happens there directly here\n' +
+				'\nRemember to start the handshake on Telegram. If you had done it, type /handshake here to finish.\n' +
+				'You can read some bits of information about me by typing /about'
+			var to = channelID ? channelID : userID
+			this.Discord_Bot.sendMessage({to: to, message: ret})
+		} else if (message.startsWith('about', 1)) {
+			var ret = 'DiteCross - Discord-Telegram Cross messaging system\n' +
+				'Developed by Giovanni Grieco - @corsaro\n' +
+				'\nI\'m open source! :D\n' +
+				'https://github.com/corsaroquad/DiteCross'
+			var to = channelID ? channelID : userID
+			this.Discord_Bot.sendMessage({to: to, message: ret})
+		} else {
+			if (!channelID) {
+				var ret = 'Sorry, what did you say?'
+				this.Discord_Bot.sendMessage({to: userID, message: ret})
+			}
+		}
 	}
-}
 
-function telegram_initialise(token) {
-	telegram_ditecross = new TelegramBot(token, {polling: true});
-
-	console.log('[Telegram] Login successful');
-
-	telegram_ditecross.on('message', function(message) {
-		//console.log('[' + message.from.username + "@" + message.chat.title + '|Telegram]> ' + message.text);
-		telegram_ditecross_serverid = message.chat.id;
-		telegram_relay_to_discord(message.from.username, message.chat.title, message.text);
-	});
-}
-
-function telegram_relay_to_discord(username, serverName, content) {
-	if (discord_ditecross_serverid) {
-		message = '[' + username + '@' + serverName + ']> ' + content;
-		discord_ditecross.sendMessage(discord_ditecross_serverid, message).then(function (message) {
-			console.log('[Telegram>>Discord] Message sent');
-		}).catch(function (error) {
-			console.log('[Telegram>>Discord] Message error');
-		});
+	this.Discord_processHandshake = function (userID, channelID) {
+		var resp = false;
+		this.Discord_handshakeRequests.forEach(function (current, index) {
+			if (
+				current[1] === this.Discord_Bot.channels[channelID].name &&
+				this.Discord_Bot.channels[channelID].type === 'text' &&
+				current[0] === this.Discord_Bot.servers[this.Discord_Bot.channels[channelID].guild_id].name &&
+				!resp
+			) {
+				this.CrossTable.push({
+					'telegram_chat_id': current[2],
+					'discord_channel_id': channelID
+				})
+				var ret = 'Handshaked with Telegram!'
+				this.Discord_Bot.sendMessage({to: channelID, message: ret})
+				ret = 'Handshaked with Discord!'
+				this.Telegram_Bot.sendMessage(current[2], ret)
+				resp = true
+			}
+		}, this)
+		if (!resp)
+			this.Discord_Bot.sendMessage({to: channelID, message: 'Sorry, I don\'t have hands to shake'})
 	}
+
+	this.Discord_manageMessage = function (userID, channelID, message) {
+		this.CrossTable.forEach(function (current, index) {
+			if (channelID === current['discord_channel_id'] && userID !== this.Discord_Bot.id) {
+				var ret = this.Discord_Bot.users[userID].username + '#' + this.Discord_Bot.channels[channelID].name + ': "' + message + '"'
+				this.Telegram_Bot.sendMessage(current['telegram_chat_id'], ret)
+			}
+		}, this)
+	}
+
+	this.Discord_API = require('discord.io')
+	this.Discord_Bot = undefined
+	this.Discord_handshakeRequests = []
+	this.Discord_initialise(Discord_Token)
+
+	/*
+	 * TELEGRAM
+	 */
+	this.Telegram_initialise = function (token) {
+		this.Telegram_Bot = new this.Telegram_API(token, {polling: true})
+		this.Telegram_Bot.onText(/@(.+)#([A-Za-z0-9\_\-]+)/, (message, reg) => this.Telegram_processHandshake(message, reg))
+		this.Telegram_Bot.on('message', (message) => this.Telegram_messagePoll(message))
+	}
+
+	this.Telegram_messagePoll = function (message) {
+		if (message.text.charAt(0) === '/')
+			this.Telegram_manageCommand(message)
+		else
+			this.Telegram_manageMessage(message)
+	}
+
+	this.Telegram_manageCommand = function (message) {
+		if (message.text.startsWith('handshake', 1)) {
+			var ret = 'What is the server and the channel you want to handshake?\n' +
+				'Please type them as @servername#channelname'
+			this.Telegram_Bot.sendMessage(message.chat.id, ret)
+			this.Telegram_handshakeRequests.push([message.chat.id, message.from.id, message.date])
+		} else if (message.text.startsWith('cancel', 1)) {
+			this.Telegram_cancelHandshake(message.chat.id)
+		} else if (message.text.startsWith('help', 1) || message.text.startsWith('start', 1)) {
+			var ret = 'I relay your messages for you on Discord or help you know what happens there directly here\n' +
+				'\nYou can control me by sending these commands:\n' +
+				'\n/handshake - associate this chat with a Discord channel\n' +
+				'/about - about me and my creator\n' +
+				'/help - get this help message\n' +
+				'/start - alias of help'
+			this.Telegram_Bot.sendMessage(message.chat.id, ret)
+		} else if (message.text.startsWith('about', 1)) {
+			var ret = 'DiteCross - Discord-Telegram Cross messaging system\n' +
+				'Developed by Giovanni Grieco - @corsaro\n' +
+				'\nI\'m open source! :D\n' +
+				'https://github.com/corsaroquad/DiteCross'
+			this.Telegram_Bot.sendMessage(message.chat.id, ret)
+		} else {
+			var ret = 'Sorry, what did you say?'
+			this.Telegram_Bot.sendMessage(message.chat.id, ret)
+		}
+	}
+
+	this.Telegram_processHandshake = function (request, regexpExec) {
+		// check if user made /handshake firstly
+		var remove_entries = [], padding = 0
+		var resp = false
+		this.Telegram_handshakeRequests.forEach(function (current, index) {
+			if (request.chat.id === current[0] && request.from.id === current[1] && !resp) {
+				resp = true;
+				remove_entries.push(index)
+			} else if ((request.chat.id === current[0] || request.from.id === current[1]) && resp) {
+				remove_entries.push(index)
+			}
+		})
+		remove_entries.forEach(function (current) {
+			this.Telegram_handshakeRequests.splice(current - padding, 1)
+			padding++
+		}, this)
+
+		if (resp) {
+			var ret = 'Please, type on that channel the command /handshake to confirm or /cancel here'
+			this.Telegram_Bot.sendMessage(request.chat.id, ret)
+
+			this.Discord_handshakeRequests.push([regexpExec[1], regexpExec[2], request.chat.id, request.date])
+		}
+	}
+
+	this.Telegram_cancelHandshake = function (chatID) {
+		var remove_entries = [], padding = 0
+		var resp = false
+		this.Discord_handshakeRequests.forEach(function (current, index) {
+			if (chatID === current[2]) {
+				resp = true
+				remove_entries.push(index)
+			}
+		})
+		remove_entries.forEach(function (current) {
+			this.Discord_handshakeRequests.splice(current - padding, 1)
+			padding++
+		}, this)
+
+		remove_entries = []
+		padding = 0
+		this.Telegram_handshakeRequests.forEach(function (current, index) {
+			if (chatID === current[0]) {
+				resp = true
+				remove_entries.push(index)
+			}
+		})
+		remove_entries.forEach(function (current) {
+			this.Discord_handshakeRequests.splice(current - padding, 1)
+			padding++
+		}, this)
+
+		var ret = ''
+		if (resp) {
+			ret = 'Handshake request canceled'
+		} else {
+			ret = 'Nothing to cancel'
+		}
+
+		this.Telegram_Bot.sendMessage(chatID, ret)
+	}
+
+	this.Telegram_manageMessage = function (message) {
+		this.CrossTable.forEach(function (current, index) {
+			if (message.chat.id === current['telegram_chat_id']) {
+				var ret = message.from.username + '@' + message.chat.title + ': "' + message.text + '"'
+				this.Discord_Bot.sendMessage({to: current['discord_channel_id'], message: ret})
+			}
+		}, this)
+	}
+
+	this.Telegram_API = require('node-telegram-bot-api')
+	this.Telegram_Bot = undefined
+	this.Telegram_handshakeRequests = [];
+	this.Telegram_initialise(Telegram_Token);
+
+	this.readCrossTable = function () {
+		var table = []
+		try {
+			table = JSON.parse(fs.readFileSync('crosstable.json', 'utf-8'))
+		} catch(error) {}
+		return table
+	}
+
+	this.CrossTable = this.readCrossTable()
+}
+
+DiteCross.prototype.saveCrossTable = function () {
+	fs.writeFileSync('crosstable.json', JSON.stringify(this.CrossTable), 'utf-8')
 }
